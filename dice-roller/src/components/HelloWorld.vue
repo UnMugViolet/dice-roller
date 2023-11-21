6d227227739db0e2264a145b3a83f1dd57d9a3a7
UnMugViolet <jaguinpaul@gmail.com>
2023-11-21 15:10:23 +0100

    💚 Build the homepage

--- dice-roller/src/components/HelloWorld.vue
+++ dice-roller/src/components/HelloWorld.vue
@@ -1,58 +1,26 @@
 <template>
-  <div class="hello bg-black">
-    <h1 class="text-white">{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="absolute bottom-0 w-full flex justify-center p-10">
+    <button class="border-2 px-5 py-2 bg-yellow-400 rounded-3xl ease-in-out throw-dice-button uppercase font-bold text-charcoal">Roll the dice</button>
   </div>
+
+  <section class="w-screen h-screen bg-coal bg-image-square">
+
+  </section>
+
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.throw-dice-button {
+  border-color: #231f20;
+  transition: all cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  filter: drop-shadow(2px 4px 0px #231f20);
+  transition: all 0.3s ease;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.throw-dice-button:hover{
+  border: 3px solid #231f20;
+  margin-bottom: 4px;
+  filter: drop-shadow(0px 0px 0px #231f20);
 }
 </style>
--- dice-roller/tailwind.config.js
+++ dice-roller/tailwind.config.js
@@ -5,8 +5,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'coal': '#0f0d0e',
+        'charcoal' : '#231f20',
+        'orange-app': '#fc7428',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function({addUtilities}) {
+      const newUtilities = {
+        '.bg-image-square': {
+          backgroundImage: 'repeating-linear-gradient(to right,#231f20,#231f20 1px,transparent 1px,transparent 30px),repeating-linear-gradient(to bottom,#231f20,#231f20 1px,transparent 1px,transparent 30px)',
+        },
+    }
+    addUtilities(newUtilities)
+  }
+  ],
 }
 
