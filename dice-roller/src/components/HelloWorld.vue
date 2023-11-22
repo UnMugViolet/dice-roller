4b744b920cc27b01fcc6052af6160da5aaad248b
UnMugViolet <jaguinpaul@gmail.com>
2023-11-22 13:12:53 +0100

    ✅ Added the die

--- dice-roller/src/components/HelloWorld.vue
+++ dice-roller/src/components/HelloWorld.vue
@@ -1,81 +1,60 @@
 <template>
   <div class="absolute bottom-0 w-full flex justify-center p-10">
     <button @click="rollDice"
-    class="border-2 px-5 py-2 bg-yellow-400 rounded-3xl throw-dice-button uppercase font-bold text-charcoal transform active:translate-y-1">Roll the dice</button>
+    class="border-2 px-5 py-2 bg-yellow-400 rounded-3xl throw-dice-button uppercase font-bold text-charcoal transform active:translate-y-1"
+    :disabled="rolling">Roll it ! </button>
   </div>
 
-  <section class="w-screen h-screen bg-coal bg-image-square">
-    <div id="dice-container" class="w-full h-full">
-
+  <section class="w-screen h-screen bg-coal bg-image-square overflow-hidden">
+    <div v-if="randomNumber" class="w-full h-full flex justify-center items-center">
+        <div class="face text-white">{{ randomNumber }}</div>
     </div>
   </section>
-
 </template>
 
-
 <script>
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { DiceD6 } from 'three/examples/jsm/geometries/DiceGeometry.js';
+import confetti from 'canvas-confetti'
 
 export default {
-  methods: {
-    rollDice() {
-      // Create a scene
-      const scene = new THREE.Scene();
-
-      // Create a camera
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.z = 5;
-
-      // Create a renderer
-      const renderer = new THREE.WebGLRenderer({ alpha: true });
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.getElementById('dice-container').appendChild(renderer.domElement);
-
-      // Load textures
-      const loader = new THREE.TextureLoader();
-      const materials = [
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-1.png') }),
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-2.png') }),
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-3.png') }),
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-4.png') }),
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-5.png') }),
-        new THREE.MeshBasicMaterial({ map: loader.load('dice-face-6.png') }),
-      ];
-
-      // Create a cube (dice)
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const cube = new THREE.Mesh(geometry, materials);
-      scene.add(cube);
-
-      // Generate a random number between 1 and 6
-      const randomNumber = Math.floor(Math.random() * 6) + 1;
-
-      // Rotate the cube to show the random number
-      cube.rotation.x += randomNumber * 0.1;
-      cube.rotation.y += randomNumber * 0.1;
-
-      // Animation loop
-      const animate = function () {
-        requestAnimationFrame(animate);
-
-        // Rotate the cube
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
-        // Render the scene
-        renderer.render(scene, camera);
-      };
-
-      animate();
+  data() {
+    return {
+      randomNumber: 0,
+      rolling: false
     }
   },
+  methods: {
+    rollDice() {
+      this.rolling = true;
+      let delay = 1000;
+      for(let i = 0; i < 20; i++) {
+        setTimeout(() => {
+          this.randomNumber = Math.floor(Math.random() * 6) + 1;
+          if(i === 19) {
+            this.randomNumber = Math.floor(Math.random() * 6) + 1;
+            setTimeout(() => {
+              confetti({
+                particleCount: 1000,
+                spread: 100,
+                origin: { y: 0.6 }
+              });
+              this.rolling = false;
+            }, 1200);
+          }
+        }, delay);
+        delay *= 0.75; // decrease delay more gradually
+      }
+    }
+  }
 }
-
 </script>
 
 <style scoped>
+
+.face {
+  font-size: 15rem;
+  font-weight: 700;
+  text-shadow: 0px 0px 10px #231f20;
+}
 
 .throw-dice-button {
   border-color: #231f20;
